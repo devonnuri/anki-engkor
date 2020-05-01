f353d337ba3d46e25e79116312441f6b453c71d2
Huiseong Yang <devonnuri@gmail.com>
2020-05-01 22:10:38 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# anki-engkor
+
+> Anki 영어-한글 생성기
+
+## Install Prerequisites
+
+1. Install pip libraries
+```
+python3 -m pip install -r requirements.txt
+```
+2. Place .db file that extracted by [apple-dictionary](https://github.com/JadedTuna/apple-dictionary) in this directory
+
+## Usage
+
+```
+python3 anki-engkor.py --list wordlist.txt --name "Word Master 2000" --output wordmaster.apkg
+```
---
+++ anki-engkor.py
@@ -0,0 +1,77 @@
+import sqlite3
+import genanki
+from xml.etree import ElementTree
+import unicodedata
+import argparse
+import random
+
+ANKI_MODEL_ID = random.randrange(1 << 30, 1 << 31)
+ANKI_DECK_ID = random.randrange(1 << 30, 1 << 31)
+
+def collate_noaccent(string1, string2):
+  norm_string1 = unicodedata.normalize('NFKD', string1).encode('ASCII', 'ignore')
+  norm_string2 = unicodedata.normalize('NFKD', string2).encode('ASCII', 'ignore')
+
+  if norm_string1 == norm_string2:
+    return 0
+  elif norm_string1 < norm_string2:
+    return 1
+  else:
+    return -1
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description='Anki Deck Creator from English-Korean Dictionary')
+  parser.add_argument('--list', required=True, help='Word list text file separated by newline')
+  parser.add_argument('--name', required=True, help='Name of Anki deck')
+  parser.add_argument('--output', required=True, help='File path of .apkg output')
+
+  args = parser.parse_args()
+
+  with open('style.css') as f:
+    css = f.read()
+
+  dict_model = genanki.Model(
+    ANKI_MODEL_ID,
+    'Dictionary Model',
+    fields=[
+      {'name': 'Word'},
+      {'name': 'Definition'},
+    ],
+    css=css,
+    templates=[
+      {
+        'name': 'Card 1',
+        'qfmt': '<div class="word">{{Word}}</div>',
+        'afmt': '{{FrontSide}}<hr id="answer">{{Definition}}',
+      },
+    ])
+
+  conn = sqlite3.connect('koreng-dictionary.db')
+  conn.create_collation('NOACCENT', collate_noaccent)
+
+  deck = genanki.Deck(ANKI_DECK_ID, args.name)
+
+  with open(args.list) as f:
+    for line in f:
+      word = line.strip()
+      print(word)
+      cursor = conn.execute('SELECT * FROM definitions WHERE title=?', (word, ))
+      row = cursor.fetchone()
+      if row is None:
+        cursor = conn.execute('SELECT * FROM definitions WHERE title=? COLLATE NOACCENT', (word, ))
+        row = cursor.fetchone()
+      if row is None:
+        print('== Word %s doesn\'t exist.' % word)
+        exit()
+      xml = row[2].decode('utf-8')
+      root = ElementTree.fromstring(xml)
+      html = ''
+      for child in root:
+        html += ElementTree.tostring(child, encoding='unicode')
+
+      note = genanki.Note(model=dict_model, fields=[word, html])
+      deck.add_note(note)
+
+  print('\n== Done!')
+
+  genanki.Package(deck).write_to_file(args.output)
